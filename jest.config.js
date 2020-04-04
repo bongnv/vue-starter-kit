41b55d511a8efe45a3b3c977a2a4f8002fe45266
Bong Nguyen <van.bong@grabtaxi.com>
2020-04-04 11:37:09 +0800

    ci: add unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+  setupFiles: ['<rootDir>/tests/unit/setup'],
+  testMatch: ['**/(*.)test.js'],
   preset: "@vue/cli-plugin-unit-jest"
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <MainLayout>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </MainLayout>
 </template>
 
 <script>
 // @ is an alias to /src
+import MainLayout from "@/layouts/MainLayout.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    MainLayout
   }
 };
 </script>
